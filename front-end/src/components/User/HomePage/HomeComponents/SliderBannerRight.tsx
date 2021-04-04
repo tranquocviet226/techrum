4401abeb02d348a6469b21b636fdaea894ae616d
viettq <viettq@vnext.com.vn>
2021-04-05 00:42:03 +0700

    VietTQ [Task] Slider Banner

--- front-end/src/components/User/HomePage/HomeComponents/SliderBannerRight.tsx
+++ front-end/src/components/User/HomePage/HomeComponents/SliderBannerRight.tsx
@@ -1,18 +1,34 @@
-import Image from 'components/User/Common/Image';
 import { connect } from 'react-redux';
+import SlickItem from './SlickItem';
 import "./SliderBannerRight.css";
+
+const item_1 = {
+    id: 2,
+    src: "https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/06/fashion_3.jpg",
+    title: "Lopez has reportedly added to her real estate holdings an eight",
+    category: [
+        {
+            id: 2,
+            name: "LIFESTYLE",
+            color: "#fda400"
+        }
+    ],
+    author: {
+        name: "DigiQuoles",
+        date: "June 30, 2021",
+        views: 3404
+    }
+}
 
 const SliderBannerRight = () => {
     return (
         <div className="sl-banner-right">
             <div className="sl-mg">
                 <div className="sl-banner-right-1">
-                    <Image src="https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/06/fashion_5.jpg"
-                        style={{ width: "100%", height: "100%", borderRadius: 8 }} />
+                    <SlickItem item={item_1} titleSize={20} />
                 </div>
                 <div className="sl-banner-right-2">
-                    <Image src="https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/06/fashion_3.jpg"
-                        style={{ width: "100%", height: "100%", borderRadius: 8 }} />
+                    <SlickItem item={item_1} titleSize={20} />
                 </div>
             </div>
         </div>
