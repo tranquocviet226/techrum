5ee44d2804cad4000bae786f8d037a25f077aea8
viettq <viettq@vnext.com.vn>
2021-04-06 19:18:19 +0700

    VietTQ [Task] Home Component

--- front-end/src/components/User/HomePage/HomeComponents/SlickSlider.tsx
+++ front-end/src/components/User/HomePage/HomeComponents/SlickSlider.tsx
@@ -3,72 +3,7 @@ import { connect } from 'react-redux';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import SlickItem from './SlickItem';
 import "./SlickSlider.css";
-
-const sliderData = [
-    {
-        id: 1,
-        src: "https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/07/fashion_1.jpg",
-        title: "The billionaire Philan thropist read to learn more and city",
-        category: [
-            {
-                id: 1,
-                name: "FASHION",
-                color: "#da1793"
-            },
-            {
-                id: 2,
-                name: "LIFESTYLE",
-                color: "#fda400"
-            }
-        ],
-        author: {
-            name: "TQV",
-            date: "June 30, 2021",
-            views: 4567
-        }
-    },
-    {
-        id: 2,
-        src: "https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/06/fashion_3.jpg",
-        title: "Lopez has reportedly added to her real estate holdings an eight",
-        category: [
-            {
-                id: 2,
-                name: "LIFESTYLE",
-                color: "#fda400"
-            }
-        ],
-        author: {
-            name: "DigiQuoles",
-            date: "June 30, 2021",
-            views: 3404
-        }
-    },
-    {
-        id: 2,
-        src: "https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/06/fashion_5.jpg",
-        title: "The billionaire Philan thropist read to learn more and city",
-        category: [
-            {
-                id: 1,
-                name: "FASHION",
-                color: "#da1793"
-            },
-            {
-                id: 2,
-                name: "LIFESTYLE",
-                color: "#fda400"
-            }
-        ],
-        author: {
-            name: "Alubaba",
-            date: "January 11, 2021",
-            views: 9999
-        }
-    }
-]
 
 const SlickSlider = () => {
     const settings = {
@@ -83,15 +18,127 @@ const SlickSlider = () => {
         autoplaySpeed: 2000
     };
     return (
-        <div className="slider-container">
-            <Slider {...settings}>
-                {sliderData.map(item => (
-                    <div key={item.id} className="sl-img-con">
-                        <SlickItem item={item} classImage="sl-img-1" titleSize={36} />
+        <Slider {...settings}>
+            <div
+                className="owl-item cloned"
+                style={{
+                    width: "732px",
+                    marginRight: "30px",
+                }}
+            >
+                <div className="ts-overlay-style featured-post  post-361 post type-post status-publish format-standard has-post-thumbnail hentry category-fashion tag-food">
+                    <div
+                        className="item item-before rocket-lazyload lazyloaded"
+                        style={{
+                            backgroundImage:
+                                'url("https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/07/fashion_1.jpg")',
+                        }}
+                        data-ll-status="loaded"
+                    >
+                        <div className="overlay-post-content">
+                            <div className="post-content">
+                                <div className="grid-category">
+                                    <a
+                                        className="post-cat"
+                                        href="https://demo.themewinter.com/wp/digiqoles/category/lifestyle/fashion/"
+                                        style={{
+                                            backgroundColor: "#da1793",
+                                            color: "#ffffff",
+                                        }}
+                                    >
+                                        Fashion
+                                                </a>
+                                </div>
+                                <h3 className="post-title">
+                                    <a href="https://demo.themewinter.com/wp/digiqoles/ratcliffe-to-be-director-of-nation-talent-trump-ignored/">
+                                        Ratcliffe to be Director of
+                                        nation talent Trump ignored
+                                                </a>
+                                </h3>
+                                <ul className="post-meta-info">
+                                    <li className="author">
+                                        <i className="fa fa-user" />
+                                        <a href="https://demo.themewinter.com/wp/digiqoles/author/digiQoles/">
+                                            duynn100198
+                                                  </a>
+                                    </li>
+                                    <li>
+                                        <i className="fa fa-clock-o" />
+                                                  July 10, 2019
+                                                </li>
+                                    <li className="active">
+                                        <i className="ts-icon ts-icon-fire" />
+                                                  3462
+                                                </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                ))}
-            </Slider>
-        </div>
+                </div>
+            </div>
+            <div
+                className="owl-item cloned"
+                style={{
+                    width: "732px",
+                    marginRight: "30px",
+                }}
+            >
+                <div className="ts-overlay-style featured-post  post-361 post type-post status-publish format-standard has-post-thumbnail hentry category-fashion tag-food">
+                    <div
+                        className="item item-before rocket-lazyload lazyloaded"
+                        style={{
+                            backgroundImage:
+                                'url("https://demo.themewinter.com/wp/digiqoles/wp-content/uploads/2019/07/fashion_1.jpg")',
+                        }}
+                        data-ll-status="loaded"
+                    >
+                        {/* <a
+                            className="img-link"
+                            href="https://demo.themewinter.com/wp/digiqoles/ratcliffe-to-be-director-of-nation-talent-trump-ignored/"
+                        /> */}
+                        <div className="overlay-post-content">
+                            <div className="post-content">
+                                <div className="grid-category">
+                                    <a
+                                        className="post-cat"
+                                        href="https://demo.themewinter.com/wp/digiqoles/category/lifestyle/fashion/"
+                                        style={{
+                                            backgroundColor: "#da1793",
+                                            color: "#ffffff",
+                                        }}
+                                    >
+                                        Fashion
+                                                </a>
+                                </div>
+                                <h3 className="post-title">
+                                    <a href="https://demo.themewinter.com/wp/digiqoles/ratcliffe-to-be-director-of-nation-talent-trump-ignored/">
+                                        Ratcliffe to be Director of
+                                        nation talent Trump ignored
+                                                </a>
+                                </h3>
+                                <ul className="post-meta-info">
+                                    <li className="author">
+                                        <i className="fa fa-user" />
+                                        <a href="https://demo.themewinter.com/wp/digiqoles/author/digiQoles/">
+                                            duynn100198
+                                                  </a>
+                                    </li>
+                                    <li>
+                                        <i className="fa fa-clock-o" />
+                                                  July 10, 2019
+                                                </li>
+                                    <li className="active">
+                                        <i className="ts-icon ts-icon-fire" />
+                                                  3462
+                                                </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Slider>
+
     )
 }
 
